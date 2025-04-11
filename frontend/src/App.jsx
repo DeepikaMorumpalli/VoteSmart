7f3acbdf3b1fdb4c15a8ba6b84ed4533eb5cc16b
Deepika <deepikareddymorumpalli@gmail.com>
2025-04-11 10:15:02 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import Welcome from './pages/Welcome';
 import RegisterPage from './pages/RegisterPage';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path={'/welcome'} element={<Welcome />} />
           <Route path={'/login'} element={<LoginPage />} />
           <Route path={'/register'} element={<RegisterPage />} />
+          <Route path={'/admin'} element={<AdminPage />} />
         </Route>
       </Routes>
     </>
